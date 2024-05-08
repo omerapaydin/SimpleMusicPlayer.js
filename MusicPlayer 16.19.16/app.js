6d74f77329893cfb3813357cf4bb65c36b0cb504
Ömer Apaydın <omerapaydin90@gmail.com>
2024-05-08 16:20:03 +0300

    First

---
+++ MusicPlayer 16.19.16/app.js
@@ -0,0 +1,60 @@
+const container = document.querySelector(".container");
+const image = document.querySelector("#music-image");
+const title = document.querySelector("#music-details .title");
+const singer = document.querySelector("#music-details .singer");
+const prev = document.querySelector("#controls #prev");
+const play = document.querySelector("#controls #play");
+const next = document.querySelector("#controls #next");
+
+const player = new MusicPlayer(musicList);
+
+let music = player.getMusic();
+
+window.addEventListener("load", () => {
+  let music = player.getMusic();
+  displayMusic(music);
+});
+
+function displayMusic(music) {
+  title.innerText = music.getName();
+  singer.innerText = music.singer;
+  image.src = "img/" + music.img;
+  audio.src = "mp3/" + music.file;
+}
+
+play.addEventListener("click", () => {
+  const isMusicPlay = container.classList.contains("playing");
+  isMusicPlay ? pauseMusic() : playMusic();
+  audio.play();
+});
+
+prev.addEventListener("click", () => {
+  prevMusic();
+});
+
+next.addEventListener("click", () => {
+  nextMusic();
+});
+
+function prevMusic() {
+  player.prev();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
+}
+function nextMusic() {
+  player.next();
+  let music = player.getMusic();
+  displayMusic(music);
+  playMusic();
+}
+function pauseMusic() {
+  container.classList.remove("playing");
+  play.classList = "fa-solid fa-play";
+  audio.pause();
+}
+function playMusic() {
+  container.classList.add("playing");
+  play.classList = "fa-solid fa-pause";
+  audio.play();
+}
